c46b1f15da09217d9d470a99a78ba1ccd5d69ab2
MatsBNL <bouw0040@hz.nl>
2022-01-20 16:38:22 +0100

    Adapter naar index

--- src/index.js
+++ src/index.js
@@ -88,3 +88,32 @@ class Steak {
       };
     }
   }
+  
+  // Adapter Class
+  class PriceAdapter {
+    constructor() {
+      const newMeat = new Chicken();
+  
+      this.operations = function(term1, term2, operation) {
+        switch (operation) {
+          case 'add':
+            // using the new implementation under the hood
+            return newMeat.add(term1, term2);
+          case 'sub':
+            return newMeat.sub(term1, term2);
+          default:
+            return NaN;
+        }
+      };
+    }
+  }
+  
+  // usage
+  const oldMeat = new Steak();
+  console.log(oldMeat.operations(10, 5, 'add')); // 15
+  
+  const newMeat = new Chicken();
+  console.log(newMeat.add(10, 5)); // 15
+  
+  const adaptedMeat = new Steacken();
+  console.log(adaptedMeat.operations(10, 5, 'add')); // 15;
