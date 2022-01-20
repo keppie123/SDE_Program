c0e473b95de0cbb8260b09192f408d0c496798b2
MatsBNL <bouw0040@hz.nl>
2022-01-20 16:37:09 +0100

    Chicken naar index

--- src/index.js
+++ src/index.js
@@ -78,4 +78,13 @@ class Steak {
       };
     }
   }
-  
+  class Chicken {
+    constructor() {
+      this.add = function(term1, term2) {
+        return term1 + term2;
+      };
+      this.sub = function(term1, term2) {
+        return term1 - term2;
+      };
+    }
+  }
