14881ec4f213da90a3c2efb5c151b26f7172b5dc
MatsBNL <bouw0040@hz.nl>
2022-01-20 16:35:11 +0100

    Steak naar index

--- src/index.js
+++ src/index.js
@@ -64,3 +64,18 @@ class Beef extends Meat {
       super(name, colour, weight);
   }
 }
+class Steak {
+    constructor() {
+      this.operations = function(term1, term2, operation) {
+        switch (operation) {
+          case 'add':
+            return term1 + term2;
+          case 'sub':
+            return term1 - term2;
+          default:
+            return NaN;
+        }
+      };
+    }
+  }
+  
