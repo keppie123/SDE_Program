0eb651860eb26e7781ab3d93af2b2170ec20e1ba
vree0028 <70525027+vree0028@users.noreply.github.com>
2022-01-20 16:31:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,61 @@ var init = () => {
 // Load the program after when the browser is ready
 window.addEventListener("load", init);
 
-import 'main.js'
+class Main {
+
+  private meats: Meat[];
+
+  public constructor() {
+      this.meats = [];
+
+      for (let i = 0; i < this.randomNumber(3, 10); i++) {
+          this.meats.push(
+              new Beef("Beef","Red", this.randomNumber(400, 600)
+              )
+          );
+      }
+
+      this.logMeats();
+  };
+
+  private logMeats () {
+      meats.forEach(element => console.log(element));
+  }
+
+  private randomNumber(min: number, max: number): number {
+      return Math.round(Math.random() * (max - min) + min);
+  }
+}
+
+abstract class Meat {
+  protected name: string;
+  protected colour: string;
+  protected weight: Number;
+
+  constructor(name: string, colour: string, weight: number) {
+      this.name = name;
+      this.colour = colour;
+      this. weight = weight;
+  }
+
+  public getName (): string {
+      return this.name;
+  }
+
+  public getColour (): string {
+      return this.colour;
+  }
+
+  public getWeight (): number {
+      return this.weight;
+  }
+}
+
+/// <reference path="Meat.ts" />
+
+class Beef extends Meat {
+    
+  constructor(name: string, colour: string, weight: number) {
+      super(name, colour, weight);
+  }
+}
